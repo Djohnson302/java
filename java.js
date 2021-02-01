175445a2ea037e02ba710b90aae7444162be5b57
Destini Johnson <djohnson302@mail.valenciacollege.edu>
2021-01-31 19:57:16 -0500

    initial commit

---
+++ java.js
@@ -0,0 +1,10 @@
+var firstName = Destini;
+console.log (firstName);
+var lastName = Johnson;
+console.log (lastName);
+var age = 24
+console.log (age)
+alert (`My name is firstName`);
+alert ("I am"+(age)+"years old");
+let statement = "I am having so much fun in this class."
+console.log (statement);
